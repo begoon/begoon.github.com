d782bce20a6667b93195df58fce09bd3ab9ee1e2
Alexander <alexander@demin.ws>
2012-04-16 21:56:19 +0100

    Added post about first books.

--- js/russian/search_loader.js
+++ js/russian/search_loader.js
@@ -3,7 +3,7 @@
         var s = document.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
-        s.src = '/js/' + language + '/search.js?2012-04-16T04:23:59+01:00';
+        s.src = '/js/' + language + '/search.js?2012-04-16T21:55:33+01:00';
         var x = document.getElementsByTagName('script')[0];
         x.parentNode.insertBefore(s, x);
     }
