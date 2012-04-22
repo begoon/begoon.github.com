4ccf31c06549cdc3541454635c1db27026000c5d
Alexander <alexander@demin.ws>
2012-04-22 23:30:48 +0100

    Typo.

--- js/english/search_loader.js
+++ js/english/search_loader.js
@@ -3,7 +3,7 @@
         var s = document.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
-        s.src = '/js/' + language + '/search.js?2012-04-22T23:04:05+01:00';
+        s.src = '/js/' + language + '/search.js?2012-04-22T23:30:37+01:00';
         var x = document.getElementsByTagName('script')[0];
         x.parentNode.insertBefore(s, x);
     }
