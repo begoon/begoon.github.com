85a3604459e53074a3a675edd682187a3f5fc79d
Alexander <alexander@demin.ws>
2012-04-29 01:36:38 +0100

    Typo.

--- js/english/search_loader.js
+++ js/english/search_loader.js
@@ -3,7 +3,7 @@
         var s = document.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
-        s.src = '/js/' + language + '/search.js?2012-04-29T01:29:46+01:00';
+        s.src = '/js/' + language + '/search.js?2012-04-29T01:36:10+01:00';
         var x = document.getElementsByTagName('script')[0];
         x.parentNode.insertBefore(s, x);
     }
