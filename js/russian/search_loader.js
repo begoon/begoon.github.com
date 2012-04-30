32305d20c42c481f0b1f88a636ccd31890ed31ac
Alexander <alexander@demin.ws>
2012-04-30 14:43:16 +0100

    Typo.

--- js/russian/search_loader.js
+++ js/russian/search_loader.js
@@ -3,7 +3,7 @@
         var s = document.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
-        s.src = '/js/' + language + '/search.js?2012-04-30T12:39:30+01:00';
+        s.src = '/js/' + language + '/search.js?2012-04-30T14:42:20+01:00';
         var x = document.getElementsByTagName('script')[0];
         x.parentNode.insertBefore(s, x);
     }
