6e6e3589ace931e1f8a45edca6e5e4f96c207eb1
Alexander <alexander@demin.ws>
2012-05-19 06:59:19 +0100

    Typos in Peter Sommerlad interview.

--- js/russian/search_loader.js
+++ js/russian/search_loader.js
@@ -3,7 +3,7 @@
         var s = document.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
-        s.src = '/js/' + language + '/search.js?2012-05-18T22:20:31+01:00';
+        s.src = '/js/' + language + '/search.js?2012-05-19T06:58:21+01:00';
         var x = document.getElementsByTagName('script')[0];
         x.parentNode.insertBefore(s, x);
     }
