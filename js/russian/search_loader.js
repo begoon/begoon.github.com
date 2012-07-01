282f2cafee2d4b93bbd260330884b967ad2a9501
Alexander <alexander@demin.ws>
2012-07-01 11:45:04 +0100

    Minor fix from Serg Vakulenko.

--- js/russian/search_loader.js
+++ js/russian/search_loader.js
@@ -3,7 +3,7 @@
         var s = document.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
-        s.src = '/js/' + language + '/search.js?2012-06-29T21:25:24+01:00';
+        s.src = '/js/' + language + '/search.js?2012-07-01T11:41:34+01:00';
         var x = document.getElementsByTagName('script')[0];
         x.parentNode.insertBefore(s, x);
     }
