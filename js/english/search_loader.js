7637cbf4dd20656cfaf976ae814d15ffa1ba3254
Alexander <alexander@demin.ws>
2012-08-02 21:57:05 +0100

    Typo.

--- js/english/search_loader.js
+++ js/english/search_loader.js
@@ -3,7 +3,7 @@
         var s = document.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
-        s.src = '/js/' + language + '/search.js?2012-07-28T20:23:20+01:00';
+        s.src = '/js/' + language + '/search.js?2012-08-02T21:51:23+01:00';
         var x = document.getElementsByTagName('script')[0];
         x.parentNode.insertBefore(s, x);
     }
