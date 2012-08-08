69809f21977a4eaea99e3017789ef53e6c91f27b
Alexander <alexander@demin.ws>
2012-08-08 23:03:05 +0100

    Post about MOXA NPort.

--- js/russian/search_loader.js
+++ js/russian/search_loader.js
@@ -3,7 +3,7 @@
         var s = document.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
-        s.src = '/js/' + language + '/search.js?2012-08-08T22:27:35+01:00';
+        s.src = '/js/' + language + '/search.js?2012-08-08T23:02:11+01:00';
         var x = document.getElementsByTagName('script')[0];
         x.parentNode.insertBefore(s, x);
     }
