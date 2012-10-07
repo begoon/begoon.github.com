5b85210f527e11c45db7a231af744b4872cd0dd5
Alexander <alexander@demin.ws>
2012-10-07 03:22:08 +0100

    Minor fixes.

--- js/english/search_loader.js
+++ js/english/search_loader.js
@@ -3,7 +3,7 @@
         var s = document.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
-        s.src = '/js/' + language + '/search.js?2012-10-07T02:54:00+01:00';
+        s.src = '/js/' + language + '/search.js?2012-10-07T03:21:48+01:00';
         var x = document.getElementsByTagName('script')[0];
         x.parentNode.insertBefore(s, x);
     }
