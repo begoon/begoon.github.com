0ad35a7305c4e4176e2ff0330fc16fbd18868f73
Alexander <alexander@demin.ws>
2012-12-10 22:37:14 +0000

    Fixed blog title.

--- js/english/search_loader.js
+++ js/english/search_loader.js
@@ -3,7 +3,7 @@
         var s = document.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
-        s.src = '/js/' + language + '/search.js?2012-12-10T22:26:00Z';
+        s.src = '/js/' + language + '/search.js?2012-12-10T22:29:14Z';
         var x = document.getElementsByTagName('script')[0];
         x.parentNode.insertBefore(s, x);
     }
