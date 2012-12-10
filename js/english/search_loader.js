fb0374923e6e81eefd875fb26ee7a14f30d61f47
Alexander <alexander@demin.ws>
2012-12-10 23:46:47 +0000

    Add tags to the post about Snake.

--- js/english/search_loader.js
+++ js/english/search_loader.js
@@ -3,7 +3,7 @@
         var s = document.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
-        s.src = '/js/' + language + '/search.js?2012-12-10T22:29:14Z';
+        s.src = '/js/' + language + '/search.js?2012-12-10T23:46:07Z';
         var x = document.getElementsByTagName('script')[0];
         x.parentNode.insertBefore(s, x);
     }
