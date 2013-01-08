95c97395d036d2f15e433402ec2a9cb20f1bc074
Alexander <alexander@demin.ws>
2013-01-08 10:17:08 +0000

    Updated the year in copyright.

--- js/russian/search_loader.js
+++ js/russian/search_loader.js
@@ -3,7 +3,7 @@
         var s = document.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
-        s.src = '/js/' + language + '/search.js?2013-01-04T08:34:32Z';
+        s.src = '/js/' + language + '/search.js?2013-01-08T10:15:45Z';
         var x = document.getElementsByTagName('script')[0];
         x.parentNode.insertBefore(s, x);
     }
