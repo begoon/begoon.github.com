751746f799b917dcd767e1816771b5902c27ddb3
Alexander <alexander@demin.ws>
2013-01-18 15:29:05 +0000

    Post thanking to Apple.

--- js/russian/search_loader.js
+++ js/russian/search_loader.js
@@ -3,7 +3,7 @@
         var s = document.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
-        s.src = '/js/' + language + '/search.js?2013-01-18T15:13:39Z';
+        s.src = '/js/' + language + '/search.js?2013-01-18T15:26:01Z';
         var x = document.getElementsByTagName('script')[0];
         x.parentNode.insertBefore(s, x);
     }
