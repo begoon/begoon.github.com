764aa130175d6281bfe8ed22c5d493eeb803eb32
Alexander <alexander@demin.ws>
2013-01-29 15:48:47 +0000

    Post about putenv memory management.

--- js/english/search_loader.js
+++ js/english/search_loader.js
@@ -3,7 +3,7 @@
         var s = document.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
-        s.src = '/js/' + language + '/search.js?2013-01-28T10:48:42Z';
+        s.src = '/js/' + language + '/search.js?2013-01-29T15:04:17Z';
         var x = document.getElementsByTagName('script')[0];
         x.parentNode.insertBefore(s, x);
     }
